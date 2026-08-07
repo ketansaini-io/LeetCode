97f4c706d8a46caa6bda5eb5deb1c166fdcbd50a
ketansaini-io <ketansaini270202007@gmail.com>
2026-08-08 00:23:18 +0530

    Time: 74 ms (5.14%), Space: 57.5 MB (25.77%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,30 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int [][]ans=new int[k][2];
-        int []arr=new int[points.length];
-        int []ind=new int[points.length];
+        int [][]arr=new int[points.length][2];
         for(int i=0;i<points.length;i++){
-            arr[i]=dis(points,i);
+            arr[i][0]=dis(points,i);
+            arr[i][1]=i;
         }
-        for(int i=0;i<points.length;i++){
-            ind[i]=i;
-        }
-        for (int i = 0; i < points.length - 1; i++) {
-        for (int j = 0; j < points.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-               int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                int tempt=ind[j];
-                ind[j]=ind[j+1];
-                ind[j+1]=tempt;
-            }
-        }
-    }
+        
+    int sorted[][]=merge(arr);
     for(int i=0;i<k;i++){
-        int x=ind[i];
-        ans[i][0]=points[x][0];
-        ans[i][1]=points[x][1];
+        ans[i][0]=points[sorted[i][1]][0];
+        ans[i][1]=points[sorted[i][1]][1];
 
     }
     return ans;
@@ -34,4 +20,45 @@ class Solution {
          int ans=(int)Math.pow(points[i][0],2)+(int)Math.pow(points[i][1],2);
          return ans;
     }
+    public static int[][] merge(int [][] arr){
+            if(arr.length==1){
+                return arr;
+            }
+            int mid=arr.length/2;
+            int[][] left=Arrays.copyOfRange(arr,0,mid);
+            int[][]right= Arrays.copyOfRange(arr, mid, arr.length);
+          return sort(merge(left),merge(right));
+        }
+    public static int[][] sort(int [][] left,int[][] right){
+            int[][] ans=new int[left.length+right.length][2];
+            int i=0;
+            int j=0;
+            while(i<left.length&&j<right.length){
+                if(left[i][0]>right[j][0]){
+                    ans[i+j][0]=right[j][0];
+                    ans[i+j][1]=right[j][1];
+                    j++;
+                }
+                else{
+                    ans[i+j][0]=left[i][0];
+                    ans[i+j][1]=left[i][1];
+                    i++;
+                }
+            }
+            if(i>=left.length){
+                while(j<right.length){
+                    ans[i+j][0]=right[j][0];
+                    ans[i+j][1]=right[j][1];
+                    j++;
+                }
+            }
+            if(j>=right.length){
+                while(i<left.length){
+                    ans[i+j][0]=left[i][0];
+                    ans[i+j][1]=left[i][1];
+                    i++;
+                }
+            }
+            return ans;
+        }    
 }
